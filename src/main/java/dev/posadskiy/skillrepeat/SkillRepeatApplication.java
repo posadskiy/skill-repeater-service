fdda2d4ff3726106aa0a5d3278a71197a1155c15
Dmitrii Posadskii <dmitry.posadsky@gmail.com>
2019-01-04 19:53:08 +0300

    Add Spring Boot 2 after initialization files

---
+++ src/main/java/dev/posadskiy/skillrepeat/SkillRepeatApplication.java
@@ -0,0 +1,14 @@
+package dev.posadskiy.skillrepeat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillRepeatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillRepeatApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/dev/posadskiy/skillrepeat/SkillRepeatApplicationTests.java
@@ -0,0 +1,17 @@
+package dev.posadskiy.skillrepeat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SkillRepeatApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
