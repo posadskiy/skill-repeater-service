aab80ae1a9a2888ec86115c759af016096691069
Dmitrii Posadskii <dmitry.posadsky@gmail.com>
2019-01-26 16:31:07 +0300

    Add update user and repeat skill functionality Use lombok annotation for generate getters and setters

--- src/main/java/dev/posadskiy/skillrepeat/db/model/Skill.java
+++ src/main/java/dev/posadskiy/skillrepeat/db/model/Skill.java
@@ -1,49 +1,19 @@
 package dev.posadskiy.skillrepeat.db.model;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+
 import java.util.Date;
 
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Skill {
+    @Id
+    private String id;
     private String name;
     private Boolean isNeedRepeat;
     private Date lastRepeat;
-    private Short level;
-
-    public Skill(String name, Boolean isNeedRepeat, Date lastRepeat, Short level) {
-        this.name = name;
-        this.isNeedRepeat = isNeedRepeat;
-        this.lastRepeat = lastRepeat;
-        this.level = level;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Boolean getNeedRepeat() {
-        return isNeedRepeat;
-    }
-
-    public void setNeedRepeat(Boolean needRepeat) {
-        isNeedRepeat = needRepeat;
-    }
-
-    public Date getLastRepeat() {
-        return lastRepeat;
-    }
-
-    public void setLastRepeat(Date lastRepeat) {
-        this.lastRepeat = lastRepeat;
-    }
-
-    public Short getLevel() {
-        return level;
-    }
-
-    public void setLevel(Short level) {
-        this.level = level;
-    }
+    private Integer level;
+    private String termRepeat;
 }
